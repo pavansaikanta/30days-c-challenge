1091480e40f3c05645cef7e8a4151b90d1366f6f
pavansaikanta <pavansaikanta1202@gmail.com>
2026-09-23 08:53:49 +0530

    Add C program for calculating student marks
    Implemented a C program to read marks for five subjects and calculate total, average, and percentage.

--- 1st challenge/challenge1.c
+++ 1st challenge/challenge1.c
@@ -1,1 +1,15 @@
-
+//a student has marks for five subjects.write a c program to read the marks of all five subjects and calculate total marks,average,percentage.display all three results clearly
+#include<stdio.h>
+int main(){
+int m1,m2,m3,m4,m5;
+float total,average,percentage;
+printf("Enter the marks for 5 subjects: ");
+scanf("%d %d %d %d %d",&m1, &m2, &m3, &m4, &m5);
+total =m1+m2+m3+m4+m5;
+average = total/5;
+percentage = (total/500)*100;
+printf("Total marks = %f\n",total);
+printf("Average marks = %f\n",average);
+printf("percentage = %f\n",percentage);
+return 0;
+}
